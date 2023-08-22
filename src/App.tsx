177ed9a75db4aa9225099d40de9d458fac9a3fc5
carlosaw <carlosfreevg@gmail.com>
2023-08-22 17:01:06 -0400

    Grid

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { GridItem } from './components/GridItem';
 
 import { GridItemType } from './types/GridItemType';
 import { items } from './data/items';
+import { formatTimeElapsed } from './helpers/formatTimeElapsed';
 
 
 const App = () => {
@@ -18,8 +19,52 @@ const App = () => {
   const [moveCount, setMoveCount] = useState<number>(0);// Movimentos
   const [shownCount, setShownCount] = useState<number>(0);// Qtos foram mostrados
   const [gridItems, setGridItems] = useState<GridItemType[]>([]);
-
+  // Criar o grid
   useEffect(() => resetAndCreateGrid(), []);
+  // Contador de tempo
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if(playing) setTimeElapsed(timeElapsed + 1);
+    }, 1000);
+    return () => clearInterval(timer);
+  }, [playing, timeElapsed]);
+  // Verificar se os abertos são iguais
+  useEffect(() => {
+    if(shownCount === 2) {
+      let opened = gridItems.filter(item => item.shown === true);
+      if(opened.length === 2) {
+        if(opened[0].item === opened[1].item) {
+          // v1 - se eles são iguais torná-los permanente
+          let tmpGrid = [...gridItems];
+          for(let i in tmpGrid) {
+            if(tmpGrid[i].shown) {
+              tmpGrid[i].permanentShown = true;
+              tmpGrid[i].shown = false;
+            }
+          }
+          setGridItems(tmpGrid);
+          setShownCount(0);
+        } else {
+          // v2 - if não forem iguais fecha os dois abertos
+          setTimeout(() => {
+            let tmpGrid = [...gridItems];
+            for(let i in tmpGrid) {
+              tmpGrid[i].shown = false;
+            }
+            setGridItems(tmpGrid);
+            setShownCount(0);
+          }, 1000);
+        }
+        setMoveCount(moveCount => moveCount + 1);
+      }
+    }
+  }, [shownCount, gridItems]);
+  // Verificar se o jogo acabou
+  useEffect(() => {
+    if(moveCount > 0 && gridItems.every(item => item.permanentShown === true)) {
+      setPlaying(false);
+    }
+  }, [moveCount, gridItems]);
 
   const resetAndCreateGrid = () => {
     // passo 1 - resetar o jogo
@@ -31,7 +76,7 @@ const App = () => {
     // 2.1 - criar um grid vazio
     let tmpGrid: GridItemType[] = [];
     for(let i = 0; i < (items.length * 2); i++) tmpGrid.push({
-      item: null, shown: true, permanentShown: false
+      item: null, shown: false, permanentShown: false
     });
     
     // 2.2 - preencher o grid
@@ -52,7 +97,15 @@ const App = () => {
   }
 
   const handleItemClick = (index: number) => {
-    
+    if(playing && index !== null && shownCount < 2) {
+      let tmpGrid = [...gridItems];
+
+      if(tmpGrid[index].permanentShown === false && tmpGrid[index].shown === false) {
+        tmpGrid[index].shown = true;
+        setShownCount(shownCount + 1);
+      }
+      setGridItems(tmpGrid);
+    }
   }
 
   return (
@@ -64,8 +117,8 @@ const App = () => {
           </C.LogoLink>
 
           <C.InfoArea>
-            <InfoItem label='Tempo' value='00:00' />
-            <InfoItem label='Movimentos' value='0' />
+            <InfoItem label='Tempo' value={formatTimeElapsed(timeElapsed)} />
+            <InfoItem label='Movimentos' value={moveCount.toString()} />
           </C.InfoArea>
 
           <Button label='Reiniciar' icon={RestartIcon} onClick={resetAndCreateGrid} />
